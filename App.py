c516050df73887ce07388d9d77b0ed2912696fe0
ReindorfN <144750275+ReindorfN@users.noreply.github.com>
2024-08-05 22:22:48 +0000

    Initial files upload

---
+++ App.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import music_bits
+
+
+
+if __name__ == "__main__":
+    text_description = st.text_input("Describe the type of beat you want to create: ").lower()
+    genre = st.text_input("Select a genre: ").lower()
+    
+    # Output file name
+    output_file = 'final_beat.wav'
+    try:
+        result = generate_music_from_text(text_description, genre)
+        print("Music generated successfully!")
+
+        #for i, items in enumerate(result):
+            #print(f'item {i}, download: {items}')
+
+        downloaded_files = download_wav_files(result)
+        print(f"Downloaded files: {downloaded_files}")
+
+        final_output = blend_wav_files(downloaded_files, output_file)
+    except Exception as e:
+        print(e)
